375ae515c6047fc905f96de78d4f8e0ea9175880
James C <james@j-chadwick.co.uk>
2025-01-29 21:19:50 +0000

    small refactor and comments

--- scripts.js
+++ scripts.js
@@ -3,8 +3,9 @@ window.addEventListener('DOMContentLoaded', () => {
   // global variables; items in the basket
   var basket = [];
 
-  // Fake API call; populate the item options on program init on program init
+  // Fake API call; populate the item options on program init
   (function () {
+    // assuming item_options is the return of an API call containing all items and corresponding data
     item_options.forEach(element => {
       document.getElementById('options').innerHTML += `<li class="options__item" data-id="${element.id}">
           ${element.item_name}
@@ -23,39 +24,53 @@ window.addEventListener('DOMContentLoaded', () => {
     const weightInputElement = document.getElementById('weight');
     const weightInput = weightInputElement.value;
 
-    // Fake API call; GET item data of item added to basket
+    // Fake API call; GET item data of item to be added to basket
     let itemData = item_options.find(item => item.id == item_id);
 
+    // for items that are priced on weight, two properties are added to the item object
+    // corresponding to the user inputted weight and price of weighed item calculated
+    // using the aforesaid inputted weight
     if (!itemData.price_per_weight || (itemData.price_per_weight && weightInput)) {
       Object.assign(itemData, weightInput ? {
         input_weight: +weightInput,
         weighed_price: +weightInput * itemData.price_per_weight
       } : {});
+      // item is added to the basket array
       basket.push(itemData);
 
       if (weightInput) {
         weightInputElement.value = '';
       }
 
+      // basket subtotal is refreshed,
+      // which leads to savings and total also being refreshed
       refreshBasket();
     }
   }
 
   // function; removes an item from the basket on basket item click in UI
   function removeFromBasket(elementPosition) {
+    // the basket item is removed from the basket array
     basket.splice(elementPosition, 1);
+
+    // basket subtotal is refreshed,
+    // which leads to savings and total also being refreshed
     refreshBasket();
   }
 
-  // function; refreshes the items in the basket and the subtotal
+  // function; refreshes the items in the basket and the subtotal,
+  // which leads to savings and total also being refreshed
   function refreshBasket() {
     const basketElement = document.getElementById('basket');
     const subTotalElement = document.getElementById('subtotal');
+    // reset subtotal ready for recalculation of basket items
     let subtotal = 0;
 
     basketElement.innerHTML = '';
     subTotalElement.textContent = '';
 
+    // cycle through each basket item, adding the item price
+    // to the subtotal (individual or weighed accordingly)
     basket.forEach(basketItem => {
       const itemPrice = basketItem.is_weighed_item ? basketItem.weighed_price : basketItem.price_per_item;
 
@@ -72,26 +87,45 @@ window.addEventListener('DOMContentLoaded', () => {
         removeFromBasket(Array.prototype.indexOf.call(basketElement.children, this));
       });
     }
+    // savings and total are calculated using the subtotal
     calculateBasket(subtotal);
   }
 
+  // function; calculates the savings and total (subtotal minus savings)
   function calculateBasket(subtotal) {
+    // savingsItems is an array of objects representing grouped similar basket items
+    // and offer details associated with those grouped items; includes the calculated
+    // value of the offer to be deducted from the subtotal
+
+    // important note: even though a group of basket items may have an associated offer,
+    // it is not automatically true that the group has accrued any savings, as the number of
+    // corresponding basket items may not be sufficient i.e. a minimum of 3 tins of beans
+    // is required to accrue savings on beans
+
+     // reset calculations
     let savingsItems = [];
     let totalSavings = 0;
     let total = 0;
 
     document.getElementById('savings').innerHTML = '';
 
+    // if basket is not empty cycle through each basket item
     if (basket.length) {
       basket.forEach(basketItem => {
+        // Fake API call; GET any offers associated with basket item
         let itemOnOffer = item_offers.find(itemOffer => itemOffer.item_id.includes(basketItem.id));
 
+        // if an offer is found from the API return, add or amend an item in the savings array to represent it
         if (itemOnOffer) {
           let savingsOffer = savingsItems.find(savingsOfferItem => savingsOfferItem.item_offer_id == itemOnOffer.id);
 
           if (savingsOffer) {
+            // if an item corresponding to the offer is already stored in the savings array,
+            // increment the number of basket items subject to offer
             savingsOffer.item_count = ++savingsOffer.item_count;
           } else {
+            // if an item corresponding to the offer is not stored in the savings array,
+            // add an savings item object to represent it
             savingsItems.push({
               item_offer_id: itemOnOffer.id,
               item_price: basketItem.price_per_item,
@@ -101,32 +135,43 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       });
 
-      savingsItems.forEach(savingsElement => {
-        let itemOffer = item_offers.find(itemOffersElement => itemOffersElement.id == savingsElement.item_offer_id);
-        let savingsValue = itemOffer.savings(savingsElement.item_count, savingsElement.item_price);
-
-        savingsElement['offer_name'] = itemOffer.offer_name;
-        savingsElement['offer_value'] = savingsValue.savings_value;
-        savingsElement['offer_count'] = savingsValue.offer_count;
-      });
-
+      // cycle through each object in the savings array
       savingsItems.forEach(savingsItem => {
-        if (savingsItem.offer_count > 0) {
-          totalSavings += savingsItem.offer_value
+        // Fake API call; GET the offer associated with saving item object
+        let itemOffer = item_offers.find(itemOffersElement => itemOffersElement.id == savingsItem.item_offer_id);
+        // calculate offer saving value using the method in the item offer object,
+        // from the number of basket items subject to the offer and the original item price
+        // only if the number of items satisfies the offer will the offer count and saving value be above zero
+        let savingsValue = itemOffer.savings(savingsItem.item_count, savingsItem.item_price);
+
+        // append offer details and savings value to saving item object even if zero,
+        // savings value is the total value of savings accrued and
+        // offer_count is the number of times the offer has been applied
+        savingsItem['offer_name'] = itemOffer.offer_name;
+        savingsItem['offer_value'] = savingsValue.savings_value;
+        savingsItem['offer_count'] = savingsValue.offer_count;
+
+        // if an offer has been applied at least once to a group of basket items,
+        // add the offer savings to the savings total
+        if (savingsValue.offer_count > 0) {
+          totalSavings += savingsValue.savings_value;
           document.getElementById('savings').innerHTML += `<li class="savings__item">
-              ${savingsItem.offer_name} x ${savingsItem.offer_count} saving £${savingsItem.offer_value.toFixed(2)}
+              ${itemOffer.offer_name} x ${savingsValue.offer_count} saving £${savingsValue.savings_value.toFixed(2)}
             </li>`;
         }
       });
     }
 
+    // calculate the total by subtracting the savings from the subtotal
     document.getElementById('savings-total').textContent = `£${totalSavings.toFixed(2).toString()}`;
     total = subtotal - totalSavings;
     document.getElementById('total').textContent = `£${total.toFixed(2).toString()}`;
   }
 });
 
-// Faux API item data
+// Faux API item data; representing each available item
+// is_weighed_item identifies items priced by weight
+// an item should only have a price_per_item or price_per_weight
 const item_options = [
   {
     id: 1,
@@ -192,7 +237,9 @@ const item_options = [
     price_per_weight: 1.99
   }
 ];
-
+// Faux API offer data; representing offers available
+// item_id is an array of items that are subject to the offer
+// savings method calculates the potential savings from an applied offer
 const item_offers = [
   {
     id: 1,
